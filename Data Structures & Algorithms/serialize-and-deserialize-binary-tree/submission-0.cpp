1065cfd27f98f87a4a0c1468336d545ab2e0cdaa
Tarpan07 <tarpansaikia@gmail.com>
2026-08-08 13:05:16 +0530

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+public:
+
+    string serialize(TreeNode* root) {
+        if (root == nullptr) {
+            return "";
+        }
+
+        stringstream ss;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* curNode = q.front();
+            q.pop();
+
+            if (curNode == nullptr) {
+                ss << "#,";
+            } else {
+                ss << curNode->val << ",";
+                q.push(curNode->left);
+                q.push(curNode->right);
+            }
+        }
+
+        return ss.str();
+    }
+
+    TreeNode* deserialize(string data) {
+                if (data.empty()) {
+            return nullptr;
+        }
+
+        stringstream s(data);
+        string str;
+        getline(s, str, ',');
+        TreeNode* root = new TreeNode(stoi(str));
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            getline(s, str, ',');
+            if (str != "#") {
+                TreeNode* leftNode = new TreeNode(stoi(str));
+                node->left = leftNode;
+                q.push(leftNode);
+            }
+
+            getline(s, str, ',');
+            if (str != "#") {
+                TreeNode* rightNode = new TreeNode(stoi(str));
+                node->right = rightNode;
+                q.push(rightNode);
+            }
+        }
+
+        return root;
+    }
+};
