07ee29abd9ceb34b70eb277c14be0fcba2985fa5
Tarpan07 <tarpansaikia@gmail.com>
2026-08-10 19:37:40 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    int getCount(int i, int amount, vector<int>& coins,
+                 vector<vector<int>>& dp) {
+
+        if (amount == 0)
+            return 0;
+
+        if (i < 0)
+            return 1e9;
+
+        if (dp[i][amount] != -1)
+            return dp[i][amount];
+
+        int take = 1e9;
+        int notTake = 1e9;
+
+        if (coins[i] <= amount) {
+            take = 1 + getCount(i, amount - coins[i], coins, dp);
+        }
+
+        notTake = getCount(i - 1, amount, coins, dp);
+
+        return dp[i][amount] = min(take, notTake);
+    }
+
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        int n = coins.size();
+
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+
+        int ans = getCount(n - 1, amount, coins, dp);
+
+        return ans == 1e9 ? -1 : ans;
+    }
+};
