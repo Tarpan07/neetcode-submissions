6b8126e9429b2004d75997826013d26f549ea181
Tarpan07 <tarpansaikia@gmail.com>
2026-08-12 01:11:50 +0530

    Add: combination-sum-iv - submission-1

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int getCount(int target, vector<int>& nums,vector<int>&dp) {
+        if (target == 0)
+            return 1;
+        if(dp[target]!=-1)return dp[target];
+        int ans = 0;
+
+        for (int num : nums) {
+            if (num <= target) {
+                ans += getCount(target - num, nums,dp);
+            }
+        }
+
+        return dp[target]=ans;
+    }
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1,-1);
+        return getCount(target,nums,dp);
+    }
+};
