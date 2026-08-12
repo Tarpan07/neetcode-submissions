d11d924e10a911a894daeae32e1b931c5f3e43ba
Tarpan07 <tarpansaikia@gmail.com>
2026-08-12 20:40:38 +0530

    Add: integer-break - submission-2

---
+++ Data Structures & Algorithms/integer-break/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    int getMax(int i,int n,vector<vector<int>>&dp){
+        if(i==0)return -1;
+        if(n==0) return 1;
+        if(dp[i][n]!=-1)return dp[i][n];
+        int take=-1;
+        if(n-i>=0)take=i*getMax(i,n-i,dp);
+        int notTake=getMax(i-1,n,dp);
+        return dp[i][n]=max(take,notTake);
+    }
+public:
+    int integerBreak(int n) {
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return getMax(n-1,n,dp);
+    }
+};
